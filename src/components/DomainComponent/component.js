431446a452e6fe9dc3f7f207104f2ee6c8fc202f
Alexander <sashaleasoc@gmail.com>
2019-09-20 20:01:32 +0300

    DomainComponent

---
+++ src/components/DomainComponent/component.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+export default function DomainName(props) {
+  return (<div className="text" >
+    <h6>Get domain</h6>
+    <h1>Get a Domain Name</h1>
+    <h3>With FREE Email, DNS, Theft Protection, and other features</h3>
+
+  <input type= 'text' ></input>
+  <button>Search Domain</button>
+
+  </div>
+  )
+}
