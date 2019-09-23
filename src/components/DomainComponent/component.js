da3a7a0e5a39ead02aec5b59f165773047e3e0e1
Roman <roma.roulin@gmail.com>
2019-09-23 20:01:15 +0300

    some styles

--- src/components/DomainComponent/component.js
+++ src/components/DomainComponent/component.js
@@ -4,17 +4,19 @@ import React from "react"
 
 export default function DomainName(props) {
   return (
-  <div className="text" >
-    <h6>Get domain</h6>
-    <h1>Get a Domain Name</h1>
-    <h3>With FREE Email, DNS, Theft Protection, and other features</h3>
+    <div className="main" >
+      <div className = "text">
+        <h6>Get domain</h6>
+        <h1>Get a Domain Name</h1>
+        <h3>With FREE Email, DNS, Theft Protection, and other features</h3>
+      </div>
+      <div className="border">
+        <input type="text" placeholder="type here" />
+        <button>Click</button>
+      </div>
 
-    <div>
-      <input type = "text" placeholder = "type here" />
+
+
     </div>
-
-
-
-  </div>
   )
 }
